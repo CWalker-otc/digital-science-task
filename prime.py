46aa38342b8250ec71049166aaafc26023f4a48f
parkero2 <parkero2@stu.otc.school.nz>
2021-03-23 15:39:06 +1300

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,5 +1,8 @@
 x = int(input("Check for a prime: "))
-for i in range(x / 2):
-    if (x % i+1 == 0):
-        print("not a prime")
-        break
+for i in range(round(x / 2)):
+    if (i != 0):
+        if (x % (i+1) == 0):
+            print("Not a prime number")
+            break
+        elif (i+1 == round(x/2)):
+            print("{} is a prime number".format(x))
